e1278c5eb1b84fae8628a1290caf7d855aa7a61e
Eric Ferreria <38568152+eferreria@users.noreply.github.com>
2023-01-23 17:11:08 -0600

    Update action_execute.py

--- action_execute.py
+++ action_execute.py
@@ -1,3 +1,7 @@
+# Copyright 2023 Google. 
+# This software is provided as-is, without warranty or representation for any use or purpose. 
+# Your use of it is subject to your agreement with Google. 
+
 # copy to main.py
 # please supply your GCS Project ID and PubSub Topic ID
 from google.cloud import storage
